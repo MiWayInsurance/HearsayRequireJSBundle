8dda1945b6c79fcfdf564d0acbe3e378d4d7b046
David du Toit <daviddut@miway.co.za>
2017-07-17 17:21:02 +0200

    Format code to fix indentation

--- Tests/Assetic/Factory/Loader/ModuleFormulaLoaderTest.php
+++ Tests/Assetic/Factory/Loader/ModuleFormulaLoaderTest.php
@@ -25,38 +25,38 @@ class ModuleFormulaLoaderTest extends \PHPUnit_Framework_TestCase
         $file2 = __DIR__ . '/dir/other_file';
 
         $factory = $this->getMockBuilder('Assetic\Factory\AssetFactory')
-                ->disableOriginalConstructor()
-                ->getMock();
+            ->disableOriginalConstructor()
+            ->getMock();
         $factory->expects($this->at(0))
-                ->method('generateAssetName')
-                ->with($file1, array())
-                ->will($this->returnValue('file'));
+            ->method('generateAssetName')
+            ->with($file1, array())
+            ->will($this->returnValue('file'));
         $factory->expects($this->at(1))
-                ->method('generateAssetName')
-                ->with($file2, array())
-                ->will($this->returnValue('other_file'));
+            ->method('generateAssetName')
+            ->with($file2, array())
+            ->will($this->returnValue('other_file'));
 
         $mapping = $this->createMock('Hearsay\RequireJSBundle\Configuration\NamespaceMappingInterface');
         $mapping->expects($this->at(0))
-                ->method('getModulePath')
-                ->with($file1)
-                ->will($this->returnValue('first/module'));
+            ->method('getModulePath')
+            ->with($file1)
+            ->will($this->returnValue('first/module'));
         $mapping->expects($this->at(1))
-                ->method('getModulePath')
-                ->with($file2)
-                ->will($this->returnValue('second/module'));
+            ->method('getModulePath')
+            ->with($file2)
+            ->will($this->returnValue('second/module'));
 
         $resource = $this->createMock('Assetic\Factory\Resource\ResourceInterface');
         $resource->expects($this->any())
-                ->method('getContent')
-                ->will($this->returnValue($file1 . "\nsome other\ntext\n" . $file2));
+            ->method('getContent')
+            ->will($this->returnValue($file1 . "\nsome other\ntext\n" . $file2));
 
         $loader = new ModuleFormulaLoader($factory, $mapping);
         $formulae = $loader->load($resource);
 
         $this->assertEquals(array(
             'file' => array($file1, array(), array('output' => 'first/module')),
-            'other_file' => array($file2, array(), array('output' => 'second/module'))
+            'other_file' => array($file2, array(), array('output' => 'second/module')),
         ), $formulae, 'Unexpected formulae loaded');
     }
 }
